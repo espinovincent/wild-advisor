d14d2850b23652905afba41c600a431ab9133165
Kamui Shirou <x_kamui@msn.com>
2023-12-16 13:15:55 +0100

    Rendering for SplashScreen

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,6 @@
+//## Define Google Maps API Key
+const apiKey = '__AIzaSyBx0oIHUf6rMkuA3QAF5o_vd9VhoT-YZF0__'
+
 //## Define most used elements in variables
 const splashscreen = document.querySelector('#splashScreen')
 const enterButton = splashscreen.querySelector('#enterButton')
