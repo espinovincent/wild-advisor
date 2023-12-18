92fc4fa31be9e7e31e5eb63e4ef62b63541731c3
Kamui Shirou <x_kamui@msn.com>
2023-12-18 19:22:34 +0100

    Getting ready to feed the 'featured' div

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,50 @@
+const featuredHomeList = document.querySelector('#featuredHomeList')
+
 function initHomePage(){
-    console.log('Get ready for magic!', foods)
+    console.log('Get ready for magic!', featured)
+
+    featured.map(feat => {
+
+        /*
+		if (datas.gems > 0) {
+            let imageSrc = 'assets/img/wayback/wayback-gems.png'
+
+			let bannerItems = document.createElement('div')
+				bannerItems.classList.add('pullItems')
+				bannerGemsShards.appendChild(bannerItems)
+
+			let bannerGems = document.createElement('img')
+            bannerGems.setAttribute('src', imageSrc)
+				bannerItems.appendChild(bannerGems)
+
+				bannerItems.innerHTML = bannerItems.innerHTML + datas.gems
+		}
+
+        
+		if (datas.shards > 0 && banner.saints[0] != undefined) {
+            let imageSrc = 'assets/img/saints/' + banner.saints[0].shard
+            
+			let bannerItems = document.createElement('div')
+				bannerItems.classList.add('pullItems')
+				bannerGemsShards.appendChild(bannerItems)
+
+			let bannerShards = document.createElement('img')
+				bannerShards.setAttribute('src', imageSrc)
+				bannerItems.appendChild(bannerShards)
+
+				bannerItems.innerHTML = bannerItems.innerHTML + datas.shards
+		}
+        */
+
+        let featDiv = document.createElement('div')
+
+        let featItm = document.createElement('li')
+            featItm.classList.add('featured')
+            featItm.appendChild(featDiv)
+
+
+        featuredHomeList.appendChild(featItm)
+
+    })
+
 }
