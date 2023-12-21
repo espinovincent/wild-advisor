dcdd2f263d86ec38816662cc45b8d3c83fe4142e
samuel-boudine-pro <152878615+samuel-boudine-pro@users.noreply.github.com>
2023-12-21 18:46:54 +0100

    Update createTag.js

--- assets/js/createTag.js
+++ assets/js/createTag.js
@@ -4,7 +4,7 @@ function createTags() {
   const cards = document.querySelector(".cards-tag");
   tags.forEach((tag) => {
     cards.innerHTML += `
-        <div class="card-tag">
+        <div class="card-tag" data-type="${tag.name}">
             <div>
                 <img src="${tag.img}" alt="${tag.name}" />
             </div>
@@ -15,3 +15,10 @@ function createTags() {
 }
 
 createTags();
+
+const cards = document.querySelector(".cards-tag");
+cards.addEventListener("click", (e) => {
+    const type = e.target.closest(".card-tag").getAttribute("data-type");
+    //const type = e.target.closest(".card-tag").dataset.type;
+    console.log(type);
+});
