4d1ecc07ddddc6e1535e3476f8d48602b76b344c
samuel-boudine-pro <152878615+samuel-boudine-pro@users.noreply.github.com>
2023-12-22 10:44:40 +0100

    Update tags.js

--- assets/js/tags.js
+++ assets/js/tags.js
@@ -1,34 +1,42 @@
 export const tags = [
   {
     name: "Pizza",
-    img: "./../assets/images/quicktags/pizza.webp",
+    img: "./../assets/images/quicktags/pizza.webp", 
+    slug:"",
   },
   {
     name: "Burger",
     img: "./../assets/images/quicktags/burger.webp",
+    slug:"",
   },
   {
     name: "Pasta",
     img: "./../assets/images/quicktags/pasta.webp",
+    slug:"",
   },
   {
     name: "Vegan",
     img: "./../assets/images/quicktags/vegan.webp",
+    slug:"",
   },
   {
     name: "Coffee",
     img: "./../assets/images/quicktags/coffee.webp",
+    slug:"",
   },
   {
     name: "Salad",
     img: "./../assets/images/quicktags/salad.webp",
+    slug:"",
   },
   {
     name: "Chicken",
     img: "./../assets/images/quicktags/chicken.webp",
+    slug:"",
   },
   {
     name: "W&B",
     img: "./../assets/images/quicktags/drinking.webp",
+    slug:"",
   },
 ];
