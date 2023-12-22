918b49e77ba54ad8d07538f654cae43f6fb663a0
samuel-boudine-pro <152878615+samuel-boudine-pro@users.noreply.github.com>
2023-12-22 10:58:55 +0100

    update app et js

--- assets/js/createTag.js
+++ assets/js/createTag.js
@@ -4,7 +4,7 @@ function createTags() {
   const cards = document.querySelector(".cards-tag");
   tags.forEach((tag) => {
     cards.innerHTML += `
-        <button type="button" class="card-tag" data-type="${tag.name}">
+        <button type="button" class="btn-card-tag" data-type="${tag.name}">
             <figure>
                 <img src="${tag.img}" alt="${tag.name}" />
             </figure>
@@ -18,7 +18,18 @@ createTags();
 
 const cards = document.querySelector(".cards-tag");
 cards.addEventListener("click", (e) => {
-    const type = e.target.closest(".card-tag").getAttribute("data-type");
+    const type = e.target.closest(".btn-card-tag").getAttribute("data-type");
     //const type = e.target.closest(".card-tag").dataset.type;
     console.log(type);
 });
+
+
+/*const btnCardTags = document.querySelectorAll(".btn-card-tag");
+btnCardTags.forEach((btnTag) => {
+  btnTag.addEventListener("click", (e) => {
+    const type = e.target.dataset.type;
+    //const type = e.target.closest(".card-tag").dataset.type;
+    console.log(type);
+  });
+});*/
+
