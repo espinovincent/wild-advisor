39a992d07072f3d4dd52444136734f1e88d3d62f
kanna 77 <kannavija@gmail.com>
2023-12-29 16:19:06 +0100

    i add js to my headline

--- assets/js/headline.js
+++ assets/js/headline.js
@@ -1,17 +1,53 @@
-function showrate(foods, featured) {
-    const rate = document.querySelector(".rate");
-    let foods = featured = []
-    return()
-}
-showrate();
+/* const titleFoods = document.createElement("h1")
+    titleFoods.innerHTML = foods.name;
+    globalInfos.classList.add("titleFood")
+    globalInfos.appendChild(globalInfos)
+   /*  let foods = featured = [] */ 
+   
 /* mettre par selection d'element showrate */
+/* function changeTitleFoods() {
+    const headlineFoods = document.getElementsByClassName('titleFoods');
+    for (let i = 0; i< foods.length; i++){
+        titleFoods[i].innerText = foods.name[0];
+    }
+    
+}
+changeTitleFoods()
+console.log(changeTitleFoods) */
 
-
-
-function foodImage () {
-    const food = document;querySelector (".foodTag")
+/* function foodImage () {
+    const food = document.querySelector (".foodTag")
     foods.map(food => {
         food.food_pic = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=800&photo_reference=${food.photos[0].photo_reference}&key=${apiKey}`
     })
     console.log(foods)
-}
+} */
+
+
+
+/* const headlineTitle = document.querySelector('.titleFoods');
+const headlineTitle = document.createElement('h1')
+      headlineTitle.textContent = headlineTitle.name */
+    
+      function createHeadline(headline) {
+
+      const imageFeatured = document.querySelector('.imageFeatured');
+      imageFeatured.src = headline.food_pic;
+
+      const headlineTitle = document.querySelector ('.titleFoods');
+      headlineTitle.textContent = headline.name;
+
+      const rate = document.querySelector ('.rate');
+      rate.textContent = headline.rating;
+
+      const dollar = document;querySelector('.dollar');
+      /* regarder la console pour extraire */
+    } 
+    createHeadline()
+    
+      /* titleFoods.addEventListener('mouseover', function() {
+        titleFoods.style.cursor = "pointer"
+    }); */
+    /* imageFeatured.addEventListener("mouseenter", function() {
+        imageFeatured.style.cursor = "crosshair"
+      }) */
