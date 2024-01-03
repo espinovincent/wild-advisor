e9d36eff4f094d8c05e8258b7722f289dff33fe7
kanna 77 <kannavija@gmail.com>
2024-01-03 17:32:51 +0100

    refresh

--- assets/js/headline.js
+++ assets/js/headline.js
@@ -8,11 +8,12 @@ function createHeadline(headline) {
 
       const rate = document.querySelector('.rate');
       /* rate.textContent = headline.rating; */
-      rate.innerHTML = `<div class="rating-score">
-                                        <span><img src="assets/images/template/star.svg" class="star" alt="${Math.trunc(feat.rating)}" title="${Math.trunc(feat.rating)}"></span>
-                                        <span>${feat.rating}</span>
-                                    </div>`
-
+      rate.innerHTML = `<div class="ratingHeadline">
+                              <span>
+                                    <img src="assets/images/template/star.svg" class="star" alt="${Math.trunc(headline.rating)}" title="${Math.trunc(headline.rating)}">
+                              </span>
+                        <span>${headline.rating}</span>
+                        </div>`
       const dollar = document.querySelector('.dollar');
       dollar.textContent = headline.price_level;
 }
