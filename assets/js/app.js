9ef0f3c9e5dc57344cc2dde3835e100794f4c681
espino vincent <tantalas95@gmail.com>
2024-01-04 16:13:44 +0100

    rectified linting

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,18 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-multi-assign */
+/* eslint-disable prefer-const */
+/* eslint-disable no-unused-vars */
 // ## Define Google Maps API Key and options
-// eslint-disable-next-line no-unused-vars
 const apiKey = 'AIzaSyBx0oIHUf6rMkuA3QAF5o_vd9VhoT-YZF0';
-// eslint-disable-next-line no-unused-vars
 const radius = 10000;
 
 // ## Define most used elements in variables
 const splashscreen = document.querySelector('#splashScreen');
-// eslint-disable-next-line no-unused-vars
 const enterButton = splashscreen.querySelector('#enterButton');
 
-// eslint-disable-next-line no-unused-vars
 const homepage = document.querySelector('#homePage');
-// eslint-disable-next-line no-unused-vars
 const featuredHomeList = document.querySelector('#featuredHomeList');
 
 // ## Items and details settled to variables
-// eslint-disable-next-line no-unused-vars, no-multi-assign, no-undef
-const foods = (featured = []);
-// eslint-disable-next-line no-unused-vars, no-multi-assign, no-undef
-const pos = (headline = null);
+let foods = featured = [];
+let pos = headline = null;
--- assets/js/tags.js
+++ assets/js/tags.js
@@ -1,4 +1,4 @@
-// eslint-disable-next-line import/prefer-default-export
+/* eslint-disable import/prefer-default-export */
 export const tags = [
   {
     name: 'Pizza',
