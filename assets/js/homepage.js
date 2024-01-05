5cf10a01c846a78c2e7e9bfeff9e5508a10bfe56
espino vincent <tantalas95@gmail.com>
2024-01-05 05:53:36 +0100

    linting

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,4 @@
+/* eslint-disable indent */
 // ## What to do while the user us busy with the SplashScreen
 //   Getting the whole app ready for him to say 'Wow', without waiting a split second
 // eslint-disable-next-line no-unused-vars
@@ -8,8 +9,8 @@ function initHomePage() {
     // TODO LATER: Add a button around the whole element, leading to the restaurant's page with details
 
     // Create figcaption containing the rate and the star
-    const featRating = document.createElement("figcaption");
-    featRating.classList.add("rating");
+    const featRating = document.createElement('figcaption');
+    featRating.classList.add('rating');
     featRating.innerHTML = `<div class="rating-score">
                                         <span><img src="assets/images/template/star.svg" class="star" alt="${Math.trunc(
                                           feat.rating,
@@ -20,30 +21,30 @@ function initHomePage() {
                                     </div>`;
 
     // Create the image and its ajdointed background
-    const featImage = document.createElement("img");
-    featImage.classList.add("cover");
-    featImage.setAttribute("src", "assets/images/template/featured-blank.webp");
-    featImage.setAttribute("alt", feat.name);
-    featImage.setAttribute("title", feat.name);
-    featImage.setAttribute("style", `background-image: url(${feat.food_pic})`);
+    const featImage = document.createElement('img');
+    featImage.classList.add('cover');
+    featImage.setAttribute('src', 'assets/images/template/featured-blank.webp');
+    featImage.setAttribute('alt', feat.name);
+    featImage.setAttribute('title', feat.name);
+    featImage.setAttribute('style', `background-image: url(${feat.food_pic})`);
 
     // Put the image and rate in the figcaption
-    const featFigure = document.createElement("figure");
+    const featFigure = document.createElement('figure');
     featFigure.appendChild(featImage);
     featFigure.appendChild(featRating);
 
     // Create the name title
-    const featName = document.createElement("h3");
+    const featName = document.createElement('h3');
     featName.textContent = feat.name;
 
     // Create the article and put the name and cover in it
-    const featArticle = document.createElement("article");
+    const featArticle = document.createElement('article');
     featArticle.appendChild(featFigure);
     featArticle.appendChild(featName);
 
     // Put everything together
-    const featItm = document.createElement("li");
-    featItm.classList.add("featured");
+    const featItm = document.createElement('li');
+    featItm.classList.add('featured');
     featItm.appendChild(featArticle);
 
     // eslint-disable-next-line no-undef
