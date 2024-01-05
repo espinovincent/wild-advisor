58b803630872be0b4bca127c6c0dcb97ca2d15ec
espino vincent <tantalas95@gmail.com>
2024-01-05 05:57:24 +0100

    linting

--- assets/js/createTag.js
+++ assets/js/createTag.js
@@ -1,8 +1,8 @@
 // eslint-disable-next-line import/extensions
-import { tags } from "./tags.js";
+import { tags } from './tags.js';
 
 function createTags() {
-  const cards = document.querySelector(".cards-tag");
+  const cards = document.querySelector('.cards-tag');
   tags.forEach((tag) => {
     cards.innerHTML += `
         <button type="button" class="btn-card-tag" data-type="${tag.name}">
@@ -24,10 +24,10 @@ cards.addEventListener("click", (e) => {
     console.log(type);
 }); */
 
-const btnCardTags = document.querySelectorAll(".btn-card-tag");
+const btnCardTags = document.querySelectorAll('.btn-card-tag');
 btnCardTags.forEach((btnTag) => {
-  btnTag.addEventListener("click", (e) => {
-    const { type } = e.target.closest(".btn-card-tag").dataset;
+  btnTag.addEventListener('click', (e) => {
+    const { type } = e.target.closest('.btn-card-tag').dataset;
     // const type = e.target.closest(".card-tag").dataset.type;
     // eslint-disable-next-line no-console
     console.log(type);
