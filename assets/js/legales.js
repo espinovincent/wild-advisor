cbe5cec2bc32c5c94f9a14ecb0ae8d1ece7edfd1
espino vincent <tantalas95@gmail.com>
2024-01-05 02:54:36 +0100

    linted

--- assets/js/legales.js
+++ assets/js/legales.js
@@ -1,19 +1,19 @@
-const popLegales = document.querySelector('#legales');
-const btnLegales = document.querySelector('#openLegales');
-const closePopin = document.querySelector('.closePopin');
+const popLegales = document.querySelector("#legales");
+const btnLegales = document.querySelector("#openLegales");
+const closePopin = document.querySelector(".closePopin");
 
 btnLegales.addEventListener(
-  'click',
+  "click",
   (e) => {
     e.preventDefault();
-    popLegales.classList.add('show');
+    popLegales.classList.add("show");
     return false;
   },
   false,
 );
 
-closePopin.addEventListener('click', (e) => {
+closePopin.addEventListener("click", (e) => {
   e.preventDefault();
-  popLegales.classList.remove('show');
+  popLegales.classList.remove("show");
   return false;
 });
